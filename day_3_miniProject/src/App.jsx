2e7032fe4b1ad78a6cf5c4b1f887c830f87155a5
dev_nick <developernikhil28@gmail.com>
2026-01-10 16:32:24 +0530

    Initial Project

--- day_3_miniProject/src/App.jsx
+++ day_3_miniProject/src/App.jsx
@@ -3,15 +3,11 @@ import './App.css'
 import Button from './component/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  let name = "Nikhil";
 
   return (
     <div className='bg-gray-700 h-screen'>
-      <h1 className='text-center text-3xl text-blue-500'>Movie List App </h1>
-      <h2 className='text-center text-2xl text-blue-950'>Username : {name}</h2>
-      <Button/>
+      <h1 className='text-center text-3xl text-blue-500'>Movie List App </h1>    
     </div>
 
   )
