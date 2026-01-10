fc0f64f9e56182c9ca63c8b60766ebbc456c6785
dev_nick <developernikhil28@gmail.com>
2026-01-10 16:23:29 +0530

    Practice code

--- day_3_miniProject/src/App.jsx
+++ day_3_miniProject/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './component/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let name = "Nikhil";
+
   return (
     <div className='bg-gray-700 h-screen'>
-      <h1 className='text-center text-3xl text-blue-500'>Movie List App</h1>
+      <h1 className='text-center text-3xl text-blue-500'>Movie List App </h1>
+      <h2 className='text-center text-2xl text-blue-950'>Username : {name}</h2>
+      <Button/>
     </div>
+
   )
 }
 
